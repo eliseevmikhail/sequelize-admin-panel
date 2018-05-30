0a968911cb3589a9c4a8c32f491169f0ed2b79a5
mikhail <eliseev@quickpay.ru>
2018-05-30 09:24:48 +0700

    minor fix, node6.0.0 compatiblity

--- src/middleware/session.js
+++ src/middleware/session.js
@@ -1,7 +1,5 @@
 module.exports = require('express-session')({
-  secret: String.fromCharCode.apply(String, Array.apply([],
-    require('crypto')
-    .randomFillSync(new Uint8Array(64)))),
+  secret: Math.random().toString(36),
   name: 'sequelize-admin-panel.sid',
   cookie: {
     httpOnly: true,
